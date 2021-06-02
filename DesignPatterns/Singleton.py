697cff115fe3c37cc97b175bc2f913b8f00c1bb1
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-06-02 16:02:43 +0200

    update

--- DesignPatterns/Singleton.py
+++ DesignPatterns/Singleton.py
@@ -8,8 +8,21 @@ class Singleton(type):
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
-    
-class Database(metaclass=Singleton):
+
+class FalseDesignPattern():
+    connection = None
+    def __init__(self):
+        self.cursor, self.connection = self.connect()
+        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS TableName (time TEXT, day INT, comment TEXT)")
+
+    def connect(self):
+        if self.connection is None:
+            self.connection = sqlite3.connect("Database.db")
+            self.cursor = self.connection.cursor()
+        return self.connection, self.cursor
+
+
+class TrueDesignPattern(metaclass=Singleton):
     connection = None
     def __init__(self):
         self.cursor, self.connection = self.connect()
@@ -23,7 +36,11 @@ class Database(metaclass=Singleton):
 
 
 if __name__ == "__main__":
-    instance_0 = Database()
-    instance_1 = Database()
-    print(f"First Instance:  {instance_0}")
-    print(f"Second Instance: {instance_1}")
+    false_instance_0 = FalseDesignPattern()
+    false_instance_1 = FalseDesignPattern()
+    true_instance_0 = TrueDesignPattern()
+    true_instance_1 = TrueDesignPattern()
+    print("- Singleton Design Pattern: Restricts the instantiation of a class to one 'single' instance.\n",
+          "This is useful when exactly one object is needed to coordinate actions across the system.\n\n",
+         f"Without Singleton: is {True if false_instance_0 == false_instance_1 else False}\n",
+         f"With Singleton:    is {True if true_instance_0 == true_instance_1 else False}\n")
