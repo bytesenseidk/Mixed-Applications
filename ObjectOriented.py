73736cc9e52856833e43273e578c3e40f20b2812
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-06-11 08:20:46 +0200

    Update ObjectOriented.py

--- ObjectOriented.py
+++ ObjectOriented.py
@@ -1,36 +1,32 @@
 
-class Dog(object):      # Parrent Class
-    race = "Pitbull"    # Class Attribute
-    def __init__(self, name, childs=0): # Constructor / Instantiator
+class Dog(object):   # Parrent Class
+    race = "Pitbull" # Class Attribute
+    def __init__(self, name): # Constructor / Instantiator
         """ Variables inside this is called instance attributes """
         self.name = name
-        # self.__childs = childs # Private attribute
-    
-    def sit(self):
-        """ Class Method for both parrent and child class """
-        print(self.name + " is sitting.")
-    
-    def bark(self):
-        print(self.name + " is barking!! ")
 
-    def __str__(self):  # String representation of the class
-        return str(f"Name: {self.name}"
-                   f"Race: {Dog.race}"
-                   f"{self.sit()}"
+    def __str__(self): # String representation of the class
+        return str(f"Name: {self.name}\n"
+                   f"Race: {Dog.race}\n"
+                   f"{self.sit()}\n"
                    f"{self.bark()}")
 
-
-# Inheritance.
-class Puppy(Dog):
-    """ Child Class """
-    def __init__(self, name):
-        """ Constructor based on parrent class """
-        super().__init__(name)
-        # Dog().__childs += 1
+    def sit(self): # Class Method
+        return str(self.name + " is sitting.")
     
     def bark(self):
-        """ Polymorphism makes you able to customize a inherrited method"""
-        print(self.name + " can't bark!.")
+        return str(self.name + " is barking!! ")
+
+
+""" Inheritance """
+class Puppy(Dog): # Child Class
+    def __init__(self, name):
+        """ Constructor based on parrent class """
+        super().__init__(name) # Inherit constructor from parrent
+    
+    def bark(self):
+        """ Polymorphism makes you able to customize a inherrited method """
+        return str(self.name + " can't bark!.")
 
 
 if __name__ == "__main__":
@@ -38,3 +34,5 @@ if __name__ == "__main__":
     sniffy = Puppy("Sniffy")  # Instance of the Puppy class.
 
     print(snoopy)
+    print("-" * 10)
+    print(sniffy)
