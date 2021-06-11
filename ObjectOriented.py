25b4f41e1188afa7c6ccf9c7226d8a10c38273f4
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-06-11 08:24:54 +0200

    Update ObjectOriented.py

--- ObjectOriented.py
+++ ObjectOriented.py
@@ -1,17 +1,17 @@
 
-class Dog(object):   # Parrent Class
-    race = "Pitbull" # Class Attribute
-    def __init__(self, name): # Constructor / Instantiator
+class Dog(object):   # Parrent Class.
+    race = "Pitbull" # Class Attribute.
+    def __init__(self, name): # Constructor / Instantiator.
         """ Variables inside this is called instance attributes """
         self.name = name
 
-    def __str__(self): # String representation of the class
+    def __str__(self): # String representation of the class.
         return str(f"Name: {self.name}\n"
                    f"Race: {Dog.race}\n"
                    f"{self.sit()}\n"
                    f"{self.bark()}")
 
-    def sit(self): # Class Method
+    def sit(self): # Class Method.
         return str(self.name + " is sitting.")
     
     def bark(self):
@@ -19,12 +19,12 @@ class Dog(object):   # Parrent Class
 
 
 """ Inheritance """
-class Puppy(Dog): # Child Class
+class Puppy(Dog): # Child Class.
     def __init__(self, name):
         """ Constructor based on parrent class """
-        super().__init__(name) # Inherit constructor from parrent
+        super().__init__(name) # Inherit constructor from parrent.
     
-    def bark(self):
+    def bark(self): # Polymorphism
         """ Polymorphism makes you able to customize a inherrited method """
         return str(self.name + " can't bark!.")
 
