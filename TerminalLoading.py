813822fd2d822fa61a140d7b62a397a9a0c3c1b7
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-07-02 20:23:12 +0200

    Update TerminalLoading.py

--- TerminalLoading.py
+++ TerminalLoading.py
@@ -1,12 +1,11 @@
 import time
 from tqdm import tqdm
 
-
 def loading():
     for _ in tqdm(range(100), desc="Loading...", ascii=False, ncols=75):
         time.sleep(0.01)
     print("Loading Done!")
 
-
 if __name__ == "__main__":
     loading()
+
