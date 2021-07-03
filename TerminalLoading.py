0053def1c9b4a910d443fbd44170dd2b7c3670af
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-07-03 17:08:21 +0200

    sad

--- TerminalLoading.py
+++ TerminalLoading.py
@@ -1,26 +1,13 @@
-import random
-from tkinter import *
+import time
+from tqdm import tqdm
 
 
-class Dice_roller(object):
-    def __init__(self, master):
-        frame = Frame(master)
-        frame.pack()
-
-        self.label = Label(master, font=("times", 200))
-        button = Button(master, text="Roll Dice!", command=self.roll)
-        button.place(x=200, y=0)
-
-    def roll(self):
-        symbols = ["\u2680", "\u2681", "\u2682", "\u2683", "\u2684", "\u2685"]
-        self.label.config(text=f"{random.choice(symbols)}{random.choice(symbols)}")
-        self.label.pack()
+def loading():
+    for _ in tqdm(range(100), desc="Loading...", ascii=False, ncols=75):
+        time.sleep(0.01)
+    print("Loading Done!")
 
 
-if __name__ == '__main__':
-    root = Tk()
-    root.title("Dice Roller")
-    root.geometry("500x300")
-    Dice_roller(root)
-    root.mainloop()
+if __name__ == "__main__":
+    loading()
 
