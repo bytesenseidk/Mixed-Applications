7c9ed7666db2d82574e512786cd61950a9535cd5
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-07-26 05:21:25 +0200

    Update SIM_Analyzer.py

--- Modules/SIM_Analyzer.py
+++ Modules/SIM_Analyzer.py
@@ -1,18 +1,35 @@
+import json
 import phonenumbers
 from phonenumbers import carrier
 from phonenumbers import geocoder
-from tabulate import tabulate
+from urllib.request import urlopen
 
-def number_scanner(phone_number):
-    number = phonenumbers.parse(phone_number)
-    description = geocoder.description_for_number(number, "en")
-    supplier = carrier.name_for_number(number, "en")
-    info = [["Country", "Supplier"],
-            [description, supplier]]
-    data = str(tabulate(info, headers="firstrow", tablefmt="github"))
-    return data
+class NumberAnalyzer(object):
+    def __init__(self, number):
+        self.number = phonenumbers.parse(number)
+        self.url = "https://ipinfo.io/"
+
+    def analyze(self):
+        description = geocoder.description_for_number(self.number, "en")
+        supplier = carrier.name_for_number(self.number, "en")
+        response = urlopen(self.url)
+        data = json.load(response)
+        return [description, supplier, data]
+    
+    def parse_data(self):
+        country, supplier, device = self.analyze()
+        region = device["region"]
+        city = device["city"]
+        location = device["loc"]
+        postal = device["postal"]
+        timezone = device["timezone"]
+        server = device["org"]
+        hostname = device["hotsname"]
+        ip = device["ip"]
+        return [country, region, city, location, postal, timezone, server, hostname, supplier, ip]
 
 if __name__ == "__main__":
     number = input("Enter Number: ")
-    print(number_scanner(number))
-
+    test = NumberAnalyzer(number)
+    print(test.parse_data())
+    
