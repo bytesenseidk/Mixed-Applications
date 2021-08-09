b93257771e2dc07d26ba9ea43ab01c350c9b67bc
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-08-09 05:21:26 +0200

    Update BarCode.py

--- Modules/BarCode.py
+++ Modules/BarCode.py
@@ -3,20 +3,32 @@ from barcode import EAN13
 from barcode.writer import ImageWriter
 from PIL import Image
 
-
 class Bar_Gen(object):
     def __init__(self, digits):
         self.bar_image = self.bar_generator(digits)
 
-
     @staticmethod
     def bar_generator(digits):
-        rv = io.BytesIO()
-        EAN13(str(digits), writer=ImageWriter()).write(rv)
-        image = Image.open(rv)
+        temp = io.BytesIO()
+        EAN13(str(digits), writer=ImageWriter()).write(temp)
+        image = Image.open(temp)
         image = image.resize((400,400), Image.ANTIALIAS)
         image.show()
 
 
 if __name__ == "__main__":
-    Bar_Gen(int(input("[BAR] Enter 12 digits: ")))
+    while True:
+        try:
+            code = int(input("[BAR] Enter 12 digits: "))
+            if code < 12 or code > 12:
+                print("Need 12 digits...")
+                continue
+            else:
+                Bar_Gen(code)
+                break
+        except KeyboardInterrupt:
+            break
+        except:
+            print("need 12 numeric values...")
+            continue
+        
