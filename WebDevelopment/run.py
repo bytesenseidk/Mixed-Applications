ab77d3151ee9e6a48fd83472cc035b1d96176b9e
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-09-01 10:08:05 +0200

    test

--- WebDevelopment/run.py
+++ WebDevelopment/run.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
+@app.route("/test")
 def home():
-    return render_template("home.html", title="Home Page")
+    return render_template("Test.html", title="Test Page")
 
 if __name__ == '__main__':
     app.run(debug=True)
