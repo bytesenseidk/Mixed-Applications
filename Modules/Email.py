b245db106f980644458f02f0799fc3598a3b57fb
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-08-05 14:00:41 +0200

    update

--- Modules/Email.py
+++ Modules/Email.py
@@ -2,13 +2,12 @@ import smtplib, ssl, getpass
 
 class Email(object):
     def __init__(self):
-        self.port = 465  # SSL port
-        self.smtp_server = "smtp.gmail.com" # smtp server address
-        creds = self.login_credentials()    # Sender credentials
-        self.sender_email = creds["email"]  # Senders email
-        self.password = creds["pass"]       # Senders password
-        # Create a secure SSL context
-        self.context = ssl.create_default_context()
+        self.port = 465                             # SSL port
+        self.smtp_server = "smtp.gmail.com"         # smtp server address
+        creds = self.login_credentials()            # Sender credentials
+        self.sender_email = creds["email"]          # Senders email
+        self.password = creds["pass"]               # Senders password
+        self.context = ssl.create_default_context() # Create a secure SSL context
 
     def login_credentials(self):
         """ The senders login credentials - password is hidden. """
@@ -20,12 +19,11 @@ class Email(object):
         """ Subject and message content """
         subject = str(input(r"Subject: "))
         message = str(input(r"Message: "))
-        return str(f"Subject: {subject}\n"
-                   f"{message}")
+        return str(f"Subject: {subject}\n{message}")
 
     def send_mail(self):
         """ Email control and forwarding """
-        receiver_email = str(input(r"Enter receivers email here: ")) # Receivers email
+        receiver_email = str(input(r"Enter receivers email here: "))
         message = self.message()
         try:
             with smtplib.SMTP_SSL(self.smtp_server, self.port, context=self.context) as server:
