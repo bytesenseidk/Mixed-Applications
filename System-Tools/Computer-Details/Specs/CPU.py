bfe977a3784f30922bcdfac23f73877b467dad3e
Byte Sensei <50791042+bytesenseidk@users.noreply.github.com>
2024-01-10 21:16:35 +0100

    fix

--- System-Tools/Computer-Details/Specs/CPU.py
+++ System-Tools/Computer-Details/Specs/CPU.py
@@ -5,7 +5,7 @@ class CentralProcessingUnit(object):
     def __init__(self):
         self.frequency = psutil.cpu_freq()
         self.data = {
-            "Physical Cores": psutil.cpu_count(logical=False)
+            "Physical Cores": psutil.cpu_count(logical=False),
             "Total Cores":  psutil.cpu_count(logical=True),
             "Max Frequency":  self.frequency.max,
             "Min Frequency":  self.frequency.min,
@@ -17,7 +17,7 @@ class CentralProcessingUnit(object):
         return str(f"Physical Cores:     {self.data['Physical Cores']}\n"
                    f"Total Cores:        {self.data['Total Cores']}\n"
                    f"Max Frequency:      {self.data['Max Frequency']}\n"
-                   f"Min Frequency:      {self.data['Min Frequency']\n"
+                   f"Min Frequency:      {self.data['Min Frequency']}\n"
                    f"Current Frequency:  {self.data['Current Frequency']}\n"
                    f"CPU Usage:          {[print(f'Core {i+1}: {percentage}%') for i, percentage  in enumerate(psutil.cpu_percent(percpu=True, interval=1))][0]}")
 
